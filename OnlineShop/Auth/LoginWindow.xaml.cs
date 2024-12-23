c6799b71a395bde79bc17f8e5c72883af9a1fec0
Dmitriy Medvedev <medvedevdm20031@gmail.com>
2024-12-23 19:04:39 +0500

    Add auth window

---
+++ OnlineShop/Auth/LoginWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace OnlineShop.Auth
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Password;
+            
+            if (username == "admin" && password == "password")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials. Please try again.");
+            }
+        }
+    }
+}
