de6c1623469db2dee939cd80883ac124c54e20bb
Dmitriy Medvedev <medvedevdm20031@gmail.com>
2025-01-08 02:04:54 +0500

    Separated styles from markup

--- OnlineShop/ProductSelection/Views/MainWindow.xaml.cs
+++ OnlineShop/ProductSelection/Views/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Controls;
 using System.Windows.Media.Animation;
 using System.Collections.Generic;
 using System.Linq;
+using MahApps.Metro.Controls;
 
 namespace OnlineShop.ProductSelection.Views
 {
